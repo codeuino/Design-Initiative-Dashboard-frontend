b7566385a8f7e6045ca9d592c5ec49c7d22f6efc
Rupeshiya <rupeshiya@gmail.com>
2020-03-27 20:47:03 +0530

    done with frontend initial code

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Posts from './Components/Posts/Posts';
+
+
+function App() {
+  return (
+    <div className="container mt-3">
+      <h1 style={{textAlign: "center"}}>Hello from Codeuino to RGSOC aspirants! 😉</h1>
+      <Posts />
+    </div>
+  );
+}
+
+export default App;
