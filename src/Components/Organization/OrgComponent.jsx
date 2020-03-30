7a4b7cca0e324e76b571102280da7746182b7718
Megha-Pathak <40709743+MPathak00@users.noreply.github.com>
2020-03-30 21:36:21 +0530

    package-json update

--- src/Components/Organization/OrgComponent.jsx
+++ src/Components/Organization/OrgComponent.jsx
@@ -7,7 +7,7 @@ import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
-import Codeuino from "../Assets/donut.png";
+import Codeuino from "../../Assets/donut.png";
 
 const useStyles = makeStyles(theme => ({
   root: {
